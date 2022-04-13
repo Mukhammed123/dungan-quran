ee657dac63ee2908ee5a526e57f84ad8293e2b72
Mukhammed123 <muhammedmusaaa@gmail.com>
2022-04-13 13:54:27 +0600

    backend initialized

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+const { Client } = require("pg");
+
+const client = new Client({
+    host: "localhost",
+    port: 5431,
+    user: "postgres",
+    password: "mukhammed123",
+    database: "quran_dungan"
+});
+
+client.connect((err: Error) => {
+    if (err) console.log(err.stack);
+    else console.log("Connected Successfully!!!");
+})
+
+console.log("Hello World")
